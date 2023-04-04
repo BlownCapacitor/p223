bc72c90856bc6d12fad21dcf64b8c0490615dda2
BlownCapacitor <52745475+BlownCapacitor@users.noreply.github.com>
2023-04-04 12:50:35 +0530

    Add files via upload

---
+++ zipword.py
@@ -0,0 +1,39 @@
+import zipfile                 
+import time
+
+folderpath = input('Path to the file: ')  
+zipf = zipfile.ZipFile(folderpath)                                 
+global result
+result = 0
+global tried
+tried = 0
+c=0
+if not zipf:
+    print('Not Encrypted') 
+
+else:
+    starttime = time.time()
+    wordListFile = open('wordlist.txt', 'r',errors='ignore')
+    body = wordListFile.read().lower()
+    words = body.split('\n')
+
+    for i in range(len(words)):
+        word = words[i]
+        password=word.encode('utf8').strip()
+        c=c+1
+        print('Trying: {}'.format(word))
+        try:
+            with zipfile.ZipFile(folderpath,'r') as zf:
+                zf.extractall(pwd=password)
+                print("Success! The password is: "+ word)
+                endtime = time.time()            
+                result = 1                      
+            break
+        except:
+             pass
+    duration = endtime - starttime
+    if(result == 0):
+        print("Sorry, password not found. A total of "+str(c)+"+ possible combinations tried in "+str(duration)+" seconds. Password is not of 4 characters.")
+    else:
+        
+        print('Congratulations! Password found after trying '+str(c)+' combinations in '+str(duration)+' seconds')
